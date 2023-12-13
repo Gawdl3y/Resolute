4a9e17ff2d89e98eab25032db51dd5ebec57ed49
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-12-12 23:29:22 -0500

    Respond to system theme changes while running

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script setup>
-import { computed, onMounted } from 'vue';
+import { ref, computed, onMounted, onUnmounted } from 'vue';
 import { invoke } from '@tauri-apps/api';
 import { info } from 'tauri-plugin-log-api';
 
@@ -18,14 +18,21 @@ import AppSidebar from './components/AppSidebar.vue';
 const settings = useSettings();
 settings.init();
 
-const theme = computed(
-	() =>
-		settings.current.theme ??
-		(window.matchMedia('(prefers-color-scheme: dark)') ? 'dark' : 'light'),
-);
+const themeMediaMatcher = window.matchMedia('(prefers-color-scheme: dark)');
+const systemTheme = ref(themeMediaMatcher.matches ? 'dark' : 'light');
+const theme = computed(() => settings.current.theme ?? systemTheme.value);
 
 onMounted(() => {
 	info('App mounted - showing main window');
 	setTimeout(() => invoke('show_window'), 50);
+	themeMediaMatcher.addEventListener('change', systemThemeListener);
 });
+
+onUnmounted(() => {
+	themeMediaMatcher.removeEventListener('change', systemThemeListener);
+});
+
+function systemThemeListener(evt) {
+	systemTheme.value = evt.matches ? 'dark' : 'light';
+}
 </script>
