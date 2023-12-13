aa281f20676f713cb725a046d949b970039fc881
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-12-13 11:36:10 -0500

    Add custom update dialog

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -4,15 +4,62 @@
 			<AppSidebar />
 			<router-view />
 		</v-layout>
+
+		<v-dialog
+			v-model="updateDialog"
+			persistent
+			scrollable
+			style="max-width: 960px"
+		>
+			<v-card
+				:title="`Resolute v${updateDetails?.version} is available!`"
+				subtitle="Would you like to install the update now?"
+			>
+				<v-card-text>
+					<div v-if="updateDetails?.notes">
+						<h3 class="text-h5">Release Notes</h3>
+						<v-divider class="mt-2" />
+						<!-- eslint-disable-next-line vue/no-v-html -->
+						<div class="release-notes pa-2" v-html="updateDetails.notes"></div>
+					</div>
+				</v-card-text>
+
+				<v-card-actions>
+					<v-spacer />
+
+					<v-btn
+						:disabled="updateDetails?.installing"
+						variant="plain"
+						@click="updateDialog = false"
+					>
+						Not now
+					</v-btn>
+					<v-btn
+						:loading="updateDetails?.installing"
+						class="text-primary font-weight-bold"
+						@click="installUpdate"
+					>
+						Update
+					</v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-dialog>
 	</v-app>
 </template>
 
 <script setup>
-import { ref, computed, onMounted, onUnmounted } from 'vue';
+import { ref, reactive, computed, onMounted, onUnmounted } from 'vue';
 import { invoke } from '@tauri-apps/api';
-import { info } from 'tauri-plugin-log-api';
+import {
+	checkUpdate as tauriCheckUpdate,
+	installUpdate as tauriInstallUpdate,
+	onUpdaterEvent as tauriOnUpdaterEvent,
+} from '@tauri-apps/api/updater';
+import { relaunch } from '@tauri-apps/api/process';
+import { info, error } from 'tauri-plugin-log-api';
 
 import useSettings from './settings';
+import { renderReleaseNotes } from './util';
 import AppSidebar from './components/AppSidebar.vue';
 
 const settings = useSettings();
@@ -22,17 +69,96 @@ const themeMediaMatcher = window.matchMedia('(prefers-color-scheme: dark)');
 const systemTheme = ref(themeMediaMatcher.matches ? 'dark' : 'light');
 const theme = computed(() => settings.current.theme ?? systemTheme.value);
 
-onMounted(() => {
+const updateDialog = ref(false);
+const updateDetails = reactive({});
+let unlistenToUpdaterEvents = null;
+
+onMounted(async () => {
 	info('App mounted - showing main window');
 	setTimeout(() => invoke('show_window'), 50);
-	themeMediaMatcher.addEventListener('change', systemThemeListener);
+
+	themeMediaMatcher.addEventListener('change', onMatchMediaChange);
+
+	unlistenToUpdaterEvents = await tauriOnUpdaterEvent(onUpdaterEvent);
+	checkForUpdate();
 });
 
 onUnmounted(() => {
-	themeMediaMatcher.removeEventListener('change', systemThemeListener);
+	themeMediaMatcher.removeEventListener('change', onMatchMediaChange);
+
+	unlistenToUpdaterEvents();
+	unlistenToUpdaterEvents = null;
 });
 
-function systemThemeListener(evt) {
+/**
+ * Handles match media change events
+ * @param {*} evt
+ */
+function onMatchMediaChange(evt) {
 	systemTheme.value = evt.matches ? 'dark' : 'light';
 }
+
+/**
+ * Checks for an app update and shows the update dialog if one is available
+ */
+async function checkForUpdate() {
+	// Check for an update
+	try {
+		const { shouldUpdate, manifest } = await tauriCheckUpdate();
+		if (!shouldUpdate) return;
+
+		info(`App update available (v${manifest.version})`);
+		console.debug('App update manifest', manifest);
+		updateDetails.manifest = manifest;
+		updateDetails.version = manifest.version;
+	} catch (err) {
+		error(`Error checking for app updates: ${err}`);
+		return;
+	}
+
+	// Render the release notes
+	try {
+		updateDetails.notes = renderReleaseNotes(updateDetails.manifest.body);
+	} catch (err) {
+		error(`Error rendering app release notes: ${err}`);
+	}
+
+	updateDialog.value = true;
+}
+
+/**
+ * Installs the available app update
+ */
+async function installUpdate() {
+	try {
+		updateDetails.installing = true;
+		await tauriInstallUpdate();
+		await relaunch();
+	} catch (err) {
+		error(`Error installing app update: ${err}`);
+	}
+}
+
+/**
+ * Handles app updater events
+ * @param {Object} data
+ */
+function onUpdaterEvent({ error, status }) {
+	if (error) error(`App updater error received (${status}): ${error}`);
+	else info(`App updater event received: ${status}`);
+}
 </script>
+
+<style>
+.release-notes h2 {
+	margin-top: 1em;
+	margin-bottom: 0.6em;
+	font-size: 1.25em;
+	font-weight: normal;
+}
+
+.release-notes ul {
+	margin-bottom: 0.5em;
+	padding-inline-start: 2em;
+}
+</style>
--- ui/src/util.js
+++ ui/src/util.js
@@ -1,3 +1,39 @@
+import { marked } from 'marked';
+import purify from 'dompurify';
+
+/**
+ * Full URL to the project's GitHub repository
+ */
+export const REPO_URL = 'https://github.com/Gawdl3y/Resolute/';
+
+/**
+ * Renders release notes from Markdown
+ * @param {string} notes
+ * @returns {string}
+ */
+export function renderReleaseNotes(notes) {
+	// Replace git hashes with Markdown links
+	notes = notes.replace(
+		/\b(([0-9a-f]{7})([0-9a-f]{1,33})?)\b/g,
+		`[$2](${REPO_URL}commit/$1)`,
+	);
+
+	// Replace issue numbers with Markdown links
+	notes = notes.replace(/(\s)(#[0-9]+)\b/g, `$1[$2](${REPO_URL}issues/$1)`);
+
+	// Set up the marked renderer to make links open in a new window
+	const renderer = new marked.Renderer();
+	const linkRenderer = renderer.link;
+	renderer.link = (href, title, text) => {
+		const html = linkRenderer.call(renderer, href, title, text);
+		return html.replace(/^<a /, '<a target="_blank" ');
+	};
+
+	// Render and sanitize the Markdown
+	const rendered = marked(notes, { renderer });
+	return purify.sanitize(rendered, { ADD_ATTR: ['target'] });
+}
+
 /**
  * Disables the context menu for a node
  * @param {Node} [node=document]
