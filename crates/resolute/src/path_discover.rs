5752047eaf7cd4b777448fcc9f7e4d7da6c5beef
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-12-15 04:04:23 -0500

    Fix Windows default Steam path and use OsStrings

--- crates/resolute/src/path_discover.rs
+++ crates/resolute/src/path_discover.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{env, path::PathBuf};
 
 use log::debug;
 use tokio::fs;
@@ -27,22 +27,30 @@ pub async fn discover_resonite() -> Result<Option<PathBuf>> {
 /// Searches for a potential Steam installation directory by reading the Windows registry or defaulting to the standard path
 #[cfg(target_os = "windows")]
 pub async fn discover_steam() -> Result<Option<PathBuf>> {
-	use winreg::{enums::HKEY_CURRENT_USER, RegKey};
+	use std::ffi::OsString;
+
+	use winreg::{enums::HKEY_CURRENT_USER, types::FromRegValue, RegKey};
 
 	// Locate a Steam installation from the registry or the default installation path
 	let steam_path = PathBuf::from({
 		let hklm = RegKey::predef(HKEY_CURRENT_USER);
-		hklm.open_subkey("Software\\Valve\\Steam")
+		hklm.open_subkey("Software\\Valve\\Steamm")
 			.and_then(|key| {
 				debug!("Opened Steam registry key, reading SteamPath value from it");
-				key.get_value("SteamPath")
+				OsString::from_reg_value(&key.get_raw_value("SteamPath")?)
 			})
 			.or_else(|err| {
 				debug!(
-					"Error reading SteamPath value from registry (falling back to default path): {}",
+					"Error reading SteamPath value from registry (trying the default path next): {}",
 					err
 				);
-				Ok::<String, Error>("%ProgramFiles(x86)%/Steam".to_owned())
+
+				// Get the program files path from the environment
+				let mut program_files = env::var_os("ProgramFiles(x86)")
+					.ok_or_else(|| Error::Path(format!("unable to get program files x86 directory: {}", err)))?;
+				program_files.push("\\Steam");
+
+				Ok::<OsString, Error>(program_files)
 			})
 			.unwrap()
 	});
@@ -59,9 +67,8 @@ pub async fn discover_steam() -> Result<Option<PathBuf>> {
 #[cfg(target_os = "linux")]
 pub async fn discover_steam() -> Result<Option<PathBuf>> {
 	// Get the user's home directory from the environment
-	let home = PathBuf::from(
-		std::env::var("HOME").map_err(|err| Error::Path(format!("unable to get home directory: {}", err)))?,
-	);
+	let home =
+		PathBuf::from(env::var_os("HOME").ok_or_else(|| Error::Path("unable to get home directory".to_owned()))?);
 
 	// Check for a traditional Steam installation
 	let traditional_path = home.join(".steam");
