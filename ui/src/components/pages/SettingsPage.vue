5b6156f526cff27e610a1027bc92ff49864eda4d
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2023-12-17 07:20:51 -0500

    Reorganize settings page into tabs

--- ui/src/components/pages/SettingsPage.vue
+++ ui/src/components/pages/SettingsPage.vue
@@ -1,42 +1,45 @@
 <template>
-	<AppHeader title="Settings" />
+	<AppHeader title="Settings">
+		<template #extension>
+			<v-tabs v-model="tab">
+				<v-tab value="general">General</v-tab>
+				<v-tab value="advanced">Advanced</v-tab>
+			</v-tabs>
+		</template>
+	</AppHeader>
 
 	<v-main>
-		<v-form>
-			<v-container>
-				<v-row>
-					<v-col>
-						<ResonitePathSetting />
+		<v-container>
+			<v-window v-model="tab">
+				<v-window-item value="general">
+					<DropdownSetting setting="theme" :items="themes" label="Theme" />
+					<ResonitePathSetting />
+					<CheckboxSetting setting="groupMods" label="Group mods by category" />
+					<v-btn @click="settings.current.setupGuideDone = false">
+						Setup guide
+					</v-btn>
+				</v-window-item>
 
-						<TextSetting
-							setting="manifestUrl"
-							:rules="[rules.url]"
-							label="Custom manifest URL"
-						/>
-
-						<DropdownSetting setting="theme" :items="themes" label="Theme" />
-
-						<CheckboxSetting
-							setting="groupMods"
-							label="Group mods by category"
-						/>
-
-						<CheckboxSetting
-							setting="modAuthorTools"
-							label="Show mod authoring tools"
-						/>
-
-						<v-btn @click="settings.current.setupGuideDone = false">
-							Show setup guide
-						</v-btn>
-					</v-col>
-				</v-row>
-			</v-container>
-		</v-form>
+				<v-window-item value="advanced">
+					<TextSetting
+						setting="manifestUrl"
+						:rules="[rules.url]"
+						label="Custom manifest URL"
+						hint="This will be used to list mods instead of the main Resonite Modding Group manifest"
+					/>
+					<CheckboxSetting
+						setting="modAuthorTools"
+						label="Show mod authoring tools"
+					/>
+				</v-window-item>
+			</v-window>
+		</v-container>
 	</v-main>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
 import useSettings from '../../composables/settings';
 import AppHeader from '../AppHeader.vue';
 import ResonitePathSetting from '../settings/ResonitePathSetting.vue';
@@ -45,6 +48,7 @@ import CheckboxSetting from '../settings/CheckboxSetting.vue';
 import DropdownSetting from '../settings/DropdownSetting.vue';
 
 const settings = useSettings();
+const tab = ref('general');
 
 /**
  * Validation rules
