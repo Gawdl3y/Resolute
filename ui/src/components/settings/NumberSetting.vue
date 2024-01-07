ec975fff573702b7b7f311b4808618bfe411a122
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2024-01-07 15:22:19 -0500

    Add setting for HTTP connection timeout

---
+++ ui/src/components/settings/NumberSetting.vue
@@ -0,0 +1,95 @@
+<template>
+	<v-text-field
+		v-model="settings.current[setting]"
+		:label="label"
+		:rules="effectiveRules"
+		:variant="variant"
+		:suffix="suffix"
+		@keypress.enter="save"
+		@blur="save"
+	/>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+import useSettings from '../../composables/settings';
+
+const props = defineProps({
+	setting: { type: String, required: true },
+	label: { type: String, required: true },
+	rules: { type: Array, default: null },
+	variant: { type: String, default: 'solo' },
+	max: { type: Number, default: Infinity },
+	min: { type: Number, default: -Infinity },
+	suffix: { type: String, default: undefined },
+});
+
+const settings = useSettings();
+
+const numberRules = {
+	float(val) {
+		if (!val) return true;
+
+		const number = Number(val);
+		if (isNaN(number)) return 'Invalid number';
+
+		return true;
+	},
+
+	min(minVal) {
+		return function min(val) {
+			if (val === null || val === '') return true;
+
+			const number = Number(val);
+			if (number < minVal) {
+				return `Minimum of ${minVal}${props.suffix ? ` ${props.suffix}` : ''}`;
+			}
+
+			return true;
+		};
+	},
+
+	max(maxVal) {
+		return function max(val) {
+			if (val === null || val === '') return true;
+
+			const number = Number(val);
+			if (number > maxVal) {
+				return `Maximum of ${maxVal}${props.suffix ? ` ${props.suffix}` : ''}`;
+			}
+
+			return true;
+		};
+	},
+};
+
+const effectiveRules = computed(() => [
+	...(props.rules ?? []),
+	numberRules.float,
+	numberRules.min(props.min),
+	numberRules.max(props.max),
+]);
+
+/**
+ * Validates then saves the text setting
+ * @return {boolean} Whether the setting was valid and saved
+ */
+async function save() {
+	const value = settings.current[props.setting];
+
+	// Validate the input
+	for (const rule of effectiveRules.value) {
+		const valid = rule(value);
+		if (!valid || typeof valid === 'string') return false;
+	}
+
+	if ((typeof value === 'string' && value) || typeof value === 'number') {
+		await settings.set(props.setting, Number(value));
+	} else {
+		await settings.unset(props.setting);
+	}
+
+	return true;
+}
+</script>
--- ui/src/composables/settings.js
+++ ui/src/composables/settings.js
@@ -8,6 +8,7 @@ let storeUnlisten;
 const currentSettings = reactive({
 	resonitePath: null,
 	manifestUrl: null,
+	connectTimeout: 10,
 	theme: null,
 	groupModIndex: true,
 	modsPerPageGrouped: -1,
@@ -50,7 +51,9 @@ export function useSettings() {
 		await store.set(setting, value);
 		info(`Setting ${setting} set to ${value}, persistNow = ${persistNow}`);
 		if (persistNow) await persist();
+
 		if (setting === 'resonitePath') invoke('resonite_path_changed');
+		else if (setting === 'connectTimeout') invoke('connect_timeout_changed');
 	}
 
 	/**
