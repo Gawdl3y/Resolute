f78b4b9b5c3b8298ce6dc9db6dbc9e901059dd15
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2024-01-08 17:44:35 -0500

    Extract copy functionality and tooltip into reusable component TextCopier

--- ui/src/components/CopyButton.vue
+++ ui/src/components/CopyButton.vue
@@ -1,48 +1,24 @@
 <template>
-	<v-tooltip
-		v-model="showTooltip"
-		:text="copied ? 'Copied!' : 'Copy'"
-		:open-delay="500"
-	>
-		<template #activator="{ props: activator }">
-			<v-slide-x-reverse-transition>
-				<v-btn
-					v-if="text && !hidden"
-					v-bind="activator"
-					variant="plain"
-					:icon="mdiContentCopy"
-					@click="copyText"
-				/>
-			</v-slide-x-reverse-transition>
-		</template>
-	</v-tooltip>
+	<TextCopier v-slot="{ props: copierProps, copy }" :text="text">
+		<v-slide-x-reverse-transition>
+			<v-btn
+				v-if="text && !hidden"
+				v-bind="{ ...copierProps, ...$attrs }"
+				variant="plain"
+				:icon="mdiContentCopy"
+				@click="copy()"
+			/>
+		</v-slide-x-reverse-transition>
+	</TextCopier>
 </template>
 
 <script setup>
-import { ref, watch } from 'vue';
 import { mdiContentCopy } from '@mdi/js';
 
-const props = defineProps({
+import TextCopier from './TextCopier.vue';
+
+defineProps({
 	text: { type: String, required: true },
 	hidden: { type: Boolean, default: false },
-});
-const copied = ref(false);
-const showTooltip = ref(false);
-
-/**
- * Writes the text to the clipboard
- */
-function copyText() {
-	navigator.clipboard.writeText(props.text);
-	copied.value = true;
-	showTooltip.value = true;
-}
-
-watch(showTooltip, (_, show) => {
-	if (show) {
-		setTimeout(() => {
-			copied.value = false;
-		}, 350);
-	}
 });
 </script>
---
+++ ui/src/components/TextCopier.vue
@@ -0,0 +1,61 @@
+<template>
+	<slot v-if="!tooltip" :copy="copy" :copied="copied" :reset="reset" />
+
+	<v-tooltip
+		v-else
+		v-model="showTooltip"
+		:text="copied ? 'Copied!' : 'Copy'"
+		:open-delay="500"
+	>
+		<template #activator="{ props: tooltipProps }">
+			<slot
+				:copy="copy"
+				:copied="copied"
+				:reset="reset"
+				:props="tooltipProps"
+			/>
+		</template>
+	</v-tooltip>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue';
+
+defineExpose({ copy, reset });
+const props = defineProps({
+	text: { type: String, required: true },
+	tooltip: { type: Boolean, default: true },
+});
+
+/**
+ * Whether the text has been copied
+ */
+const copied = ref(false);
+
+/**
+ * Whether the tooltip is being shown
+ */
+const showTooltip = ref(false);
+
+// Reset the copied state whenever the tooltip closes
+watch(showTooltip, (show) => {
+	if (!show) setTimeout(reset, 350);
+});
+
+/**
+ * Writes the text to the clipboard and sets the copied state
+ * @param {boolean} [forceShowTooltip=true]
+ */
+function copy(forceShowTooltip = true) {
+	navigator.clipboard.writeText(props.text);
+	copied.value = true;
+	if (forceShowTooltip) showTooltip.value = true;
+}
+
+/**
+ * Resets the copied state
+ */
+function reset() {
+	copied.value = false;
+}
+</script>
